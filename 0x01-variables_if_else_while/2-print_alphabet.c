36319e993b36c81185e263940254177f6aa1c572
Allegra <allyitambala@gmail.com>
2022-09-09 11:46:07 -0700

    1-3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <studio.h>
+
+/**
+  * main -Entry point
+  *
+  * Description: print all alphabet letters
+  *
+  * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= '2')
+	{
+		putchar(ch);
+		++ch;
+	}
+	putchar('\n');
+
+	return (0);
+
+}

306dfcea7f52cf581396bedf56ba12df7ebf82a5
Allegra <allyitambala@gmail.com>
2022-09-14 15:03:17 -0700

    betty

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -5,13 +5,12 @@
     *       value of an integer
      *
       * @n: takes in integer type input for function
-       *
-        * Return: Always 0 (Success)
+       * * Return: Always 0 (Success)
 	*/
 
 int _abs(int n)
 {
 	if (n < 0)
-		n = (-1) * n
-			return (n);
+		n = (-1) * n;
+	return (n);
 }
